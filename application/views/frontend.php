d94d50663930c130c742dfd6b822e346c50254a9
sayuticom <sayuti.com.oke@gmail.com>
2025-12-31 22:35:31 +0700

    Update frontend.php

--- application/views/frontend.php
+++ application/views/frontend.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>ACT – Ayoo Cepat Tolong</title>
+		<title>ACT – Ayoo Tolong</title>
 		<link rel="icon" href="<?= base_url('assets/img/favicon.png') ?>">
 		<script src="https://cdn.tailwindcss.com"></script>
 		
